b986283ccab1a7f709ec9b462bbfdead48a73749
Phash <mroedig@gmail.com>
2018-08-02 19:33:40 +0200

    Service eingeführt
    Signed-off-by: Phash <mroedig@gmail.com>

--- app/src/main/java/de/phash/manuel/mathetrainer/EinfachesRechnen.kt
+++ app/src/main/java/de/phash/manuel/mathetrainer/EinfachesRechnen.kt
@@ -57,22 +57,9 @@ class EinfachesRechnen : AppCompatActivity() {
     }
 
     private fun increaseAllStates() {
-        Status.instance.richtigAdd()
-        Status.instance.versucheAdd()
-        Status.instance.aufgabeAdd()
-        val sharedPref = this.getSharedPreferences("de.phash.manuel.mathetrainer", Context.MODE_PRIVATE)
-        var richtigPers = sharedPref.getInt(Status.RICHTIGE, 0)
-        sharedPref.edit().putInt(Status.RICHTIGE, richtigPers).apply()
-        var versuchPers = sharedPref.getInt(Status.VERSUCHE, 0)
-        sharedPref.edit().putInt(Status.VERSUCHE, versuchPers).apply()
-        var aufgabePers = sharedPref.getInt(Status.AUFGABEN, 0)
-        sharedPref.edit().putInt(Status.AUFGABEN, aufgabePers).apply()
-
-        sharedPref.edit().putInt(Status.AUFGABENALLTIME, (sharedPref.getInt(Status.AUFGABENALLTIME, 0) + 1)).apply()
-        sharedPref.edit().putInt(Status.VERSUCHEALLTIME, (sharedPref.getInt(Status.VERSUCHEALLTIME, 0) + 1)).apply()
-        sharedPref.edit().putInt(Status.RICHTIGEALLTIME, (sharedPref.getInt(Status.RICHTIGEALLTIME, 0) + 1)).apply()
-
+        StatsUpdateService.instance.increaseAllStates(this.getSharedPreferences("de.phash.manuel.mathetrainer", Context.MODE_PRIVATE))
     }
+
     private fun updateVersuche() {
         val versucheView = findViewById(R.id.versucheView) as TextView
         versucheView.text = Status.instance.versuche.toString()
---
+++ app/src/main/java/de/phash/manuel/mathetrainer/StatsUpdateService.kt
@@ -0,0 +1,33 @@
+package de.phash.manuel.mathetrainer
+
+import android.content.SharedPreferences
+
+class StatsUpdateService private constructor() {
+    companion object {
+        val instance: StatsUpdateService by lazy { StatsUpdateService.Holder.INSTANCE }
+    }
+
+    private object Holder {
+        val INSTANCE = StatsUpdateService()
+    }
+
+    init {
+        println("This ($this) is a singleton")
+    }
+
+    fun increaseAllStates(sharedPref: SharedPreferences) {
+        Status.instance.richtigAdd()
+        Status.instance.versucheAdd()
+        Status.instance.aufgabeAdd()
+        var richtigPers = sharedPref.getInt(Status.RICHTIGE, 0)
+        sharedPref.edit().putInt(Status.RICHTIGE, richtigPers).apply()
+        var versuchPers = sharedPref.getInt(Status.VERSUCHE, 0)
+        sharedPref.edit().putInt(Status.VERSUCHE, versuchPers).apply()
+        var aufgabePers = sharedPref.getInt(Status.AUFGABEN, 0)
+        sharedPref.edit().putInt(Status.AUFGABEN, aufgabePers).apply()
+
+        sharedPref.edit().putInt(Status.AUFGABENALLTIME, (sharedPref.getInt(Status.AUFGABENALLTIME, 0) + 1)).apply()
+        sharedPref.edit().putInt(Status.VERSUCHEALLTIME, (sharedPref.getInt(Status.VERSUCHEALLTIME, 0) + 1)).apply()
+        sharedPref.edit().putInt(Status.RICHTIGEALLTIME, (sharedPref.getInt(Status.RICHTIGEALLTIME, 0) + 1)).apply()
+    }
+}
