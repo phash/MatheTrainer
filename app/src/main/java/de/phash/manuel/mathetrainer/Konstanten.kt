9a43000e42cd21dfd99ee5225afbe67f7f770512
Phash <mroedig@gmail.com>
2018-08-02 22:23:34 +0200

    Schwierigkeitsstufen eingeführt
    Signed-off-by: Phash <mroedig@gmail.com>

--- app/src/main/java/de/phash/manuel/mathetrainer/Konstanten.kt
+++ app/src/main/java/de/phash/manuel/mathetrainer/Konstanten.kt
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2018 Manuel "Phash" Roedig
+ *
+ * Distributed under the MIT software license, see the accompanying file
+ * LICENSE or https://opensource.org/licenses/mit-license.php
+ */
 package de.phash.manuel.mathetrainer
 
 class Konstanten {
--- app/src/main/java/de/phash/manuel/mathetrainer/MatheAufgaben.java
+++ app/src/main/java/de/phash/manuel/mathetrainer/MatheAufgaben.java
@@ -1,5 +1,10 @@
+/**
+ * Copyright (c) 2018 Manuel "Phash" Roedig
+ * <p>
+ * Distributed under the MIT software license, see the accompanying file
+ * LICENSE or https://opensource.org/licenses/mit-license.php
+ */
 package de.phash.manuel.mathetrainer;
-
 
 import org.apache.commons.lang3.RandomUtils;
 
